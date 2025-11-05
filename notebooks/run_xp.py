66609686de9e254eff971bc02ef78803aa2afabd
vincent.auriau@centralesupelec.fr <auriauvi@ruche02.cluster>
2025-11-05 17:32:04 +0100

    ready for slurming

--- notebooks/run_xp.py
+++ notebooks/run_xp.py
@@ -1,6 +1,12 @@
 import sys
 
+print(sys.executable)
+print("ole")
+print(sys.version)
+import matplotlib.pyplot as plt
+import gurobipy as hp
 sys.path.append("../")
+
 
 import pickle
 import numpy as np
@@ -11,6 +17,12 @@ from python.distances import TwoUTASpaceDiameter
 alldist = {}
 for ndata in [10, 20, 100, 1_000]:
     print("Nb of data:", ndata)
+    generator = SyntheticDataGenerator(
+    	n_dms=2,
+    	n_criteria=4,
+    	method_params={"n_pieces": 5},
+	)
+
     X, Y, info = generator.generate_preferences(num_pairs=ndata, return_clusters=True)
     dist = TwoUTASpaceDiameter(n_pieces=5)
     dist.fit(X, Y)
