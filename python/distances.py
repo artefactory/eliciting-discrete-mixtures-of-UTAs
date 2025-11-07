25ee07ae34f15bb561d7768e9a11e1ac19d97cff
VincentAuriau <auriau.vincent@gmail.com>
2025-11-07 11:57:39 +0100

    ADD: 2-UTA space diameter

--- python/distances.py
+++ python/distances.py
@@ -1016,7 +1016,7 @@ class TwoUTASpaceDiameter(object):
 
         if verbose > 1:
             print("1/ Variables Definition")
-        self.marginal_coeffs = {
+        self.marginal_coeffs = {    
             (name, i, j): self.solver.addVar(vtype=gp.GRB.CONTINUOUS, name=f"s_{name}_{i}_{j}")
             for j in range(self.n_pieces + 1)
             for i in range(n_features)
@@ -1234,7 +1234,7 @@ class TwoUTASpaceDiameter(object):
 
 
     def fit_from_indifferences(
-        self, X, Y, ref_coeffs, sample_weight=None, time_limit=None, verbose=0, majoring_value=10,
+        self, X, Y, sample_weight=None, time_limit=None, verbose=0, majoring_value=10,
     ):
         """Estimation of the parameters"""
         n_samples = X.shape[0]
@@ -1249,155 +1249,201 @@ class TwoUTASpaceDiameter(object):
 
         if verbose > 1:
             print("1/ Variables Definition")
-        marginal_coeffs = {
-            (i, j): self.solver.addVar(vtype=gp.GRB.CONTINUOUS, name=f"s_{i}_{j}")
+        self.marginal_coeffs = {    
+            (name, i, j): self.solver.addVar(vtype=gp.GRB.CONTINUOUS, name=f"s_{name}_{i}_{j}")
             for j in range(self.n_pieces + 1)
             for i in range(n_features)
+            for name in ["d1", "d2", "s1", "s2"]
         }
 
         self.abs_vals = {
-            (i, j): self.solver.addVar(vtype=gp.GRB.CONTINUOUS, name=f"absval_{i}_{j}")
+            (name, i, j): self.solver.addVar(vtype=gp.GRB.CONTINUOUS, name=f"absval_{name}_{i}_{j}")
             for j in range(self.n_pieces + 1)
             for i in range(n_features)
+            for name in ["d1_s1", "d1_s2", "d2_s1", "d2_s2"]
         }
         self.abs_id = {
-            (i, j): self.solver.addVar(vtype=gp.GRB.BINARY, name=f"absid_{i}_{j}")
+            (name, i, j): self.solver.addVar(vtype=gp.GRB.BINARY, name=f"absid_{name}_{i}_{j}")
             for j in range(self.n_pieces + 1)
             for i in range(n_features)
+            for name in ["d1_s1", "d1_s2", "d2_s1", "d2_s2"]
         }
 
-        self.marginal_coeffs = marginal_coeffs
-        estimate_x = {
-            (i, j): self.solver.addVar(
-                vtype=gp.GRB.CONTINUOUS, name=f"estimate_x_{i}_{j}"
+        self.estimate_x = {
+            (name, i, j): self.solver.addVar(
+                vtype=gp.GRB.CONTINUOUS, name=f"estimate_x_{name}_{i}_{j}"
             )
             for j in range(n_features)
             for i in range(n_samples)
+            for name in ["d1", "d2", "s1", "s2"]
         }
-        estimate_y = {
-            (i, j): self.solver.addVar(
-                vtype=gp.GRB.CONTINUOUS, name=f"estimate_y_{i}_{j}"
+        self.estimate_y = {
+            (name, i, j): self.solver.addVar(
+                vtype=gp.GRB.CONTINUOUS, name=f"estimate_y_{name}_{i}_{j}"
             )
             for j in range(n_features)
             for i in range(n_samples)
+            for name in ["d1", "d2", "s1", "s2"]
+        }
+
+        self.z_s = {
+            k: self.solver.addVar(
+                vtype=gp.GRB.BINARY, name=f"z_s_{k}"
+            )
+            for k in range(n_samples)
+        }
+        self.z_d = {
+            k: self.solver.addVar(
+                vtype=gp.GRB.BINARY, name=f"z_d_{k}"
+            )
+            for k in range(n_samples)
         }
 
         if verbose > 1:
             print("2/ Constraints Definition")
         # [MI - 2]
-        lower_bound = {
-            i: self.solver.addConstr(
-                marginal_coeffs[i, 0] == 0, name="lower_bound_normalization"
-            )
-            for i in range(n_features)
-        }
-        sum_to_one = {
-            self.solver.addConstr(
-                (
-                    gp.quicksum(
-                        marginal_coeffs[i, self.n_pieces] for i in range(n_features)
-                    )
+        for name in ["d1", "d2", "s1", "s2"]:
+            zero_bound = {
+                i: self.solver.addConstr(
+                    self.marginal_coeffs[name, i, 0] == 0, name="lower_bound_normalization"
                 )
-                == 1,
-                name="sum_to_one",
-            )
-        }
-        monotonicity = {
-            (i): {
-                k: self.solver.addConstr(
-                    marginal_coeffs[i, k + 1] >= marginal_coeffs[i, k],
-                    name="monotonicity",
-                )
-                for k in range(self.n_pieces)
+                for i in range(n_features)
             }
-            for i in range(n_features)
-        }
-        # [MI - 4]
-        # [MI - 1']
-        # [MI - 8']
-        if verbose > 1:
-            print("3/ Utility Constraints")
-        # Contraintes de préférences
-        for i in range(n_samples):
-            for j in range(n_features):
-                for k in range(self.n_pieces):
-                    if self.inflexions[j][k] <= X[i][j] <= self.inflexions[j][k + 1]:
-                        self.solver.addConstr(
-                            estimate_x[i, j]
-                            == marginal_coeffs[j, k]
-                            + (
-                                (X[i][j] - self.inflexions[j][k])
-                                / (self.inflexions[j][k + 1] - self.inflexions[j][k])
-                            )
-                            * (marginal_coeffs[j, k + 1] - marginal_coeffs[j, k]),
-                            name=f"estimate_x_{i}_{j}",
+            sum_to_one = {
+                self.solver.addConstr(
+                    (
+                        gp.quicksum(
+                            self.marginal_coeffs[name, i, self.n_pieces] for i in range(n_features)
                         )
-                    if self.inflexions[j][k] <= Y[i][j] <= self.inflexions[j][k + 1]:
-                        self.solver.addConstr(
-                            estimate_y[i, j]
-                            == marginal_coeffs[j, k]
-                            + (
-                                (Y[i][j] - self.inflexions[j][k])
-                                / (self.inflexions[j][k + 1] - self.inflexions[j][k])
+                    )
+                    == 1,
+                    name="sum_to_one",
+                )
+            }
+            monotonicity = {
+                (i): {
+                    k: self.solver.addConstr(
+                        self.marginal_coeffs[name, i, k + 1] >= self.marginal_coeffs[name, i, k],
+                        name="monotonicity",
+                    )
+                    for k in range(self.n_pieces)
+                }
+                for i in range(n_features)
+            }
+            # [MI - 4]
+            # [MI - 1']
+            # [MI - 8']
+            if verbose > 1:
+                print("3/ Utility Constraints")
+            # Contraintes de préférences
+
+            for i in range(n_samples):
+                for j in range(n_features):
+                    for k in range(self.n_pieces):
+                        if self.inflexions[j][k] <= X[i][j] <= self.inflexions[j][k + 1]:
+                            self.solver.addConstr(
+                                self.estimate_x[name, i, j]
+                                == self.marginal_coeffs[name, j, k]
+                                + (
+                                    (X[i][j] - self.inflexions[j][k])
+                                    / (self.inflexions[j][k + 1] - self.inflexions[j][k])
+                                )
+                                * (self.marginal_coeffs[name, j, k + 1] - self.marginal_coeffs[name, j, k]),
+                                name=f"estimate_x_{i}_{j}",
                             )
-                            * (marginal_coeffs[j, k + 1] - marginal_coeffs[j, k]),
-                            name=f"estimate_y_{i}_{j}",
-                        )
-        # Indifferences
-        pref_A = {
-            (i): self.solver.addConstr(
-                gp.quicksum([estimate_x[(i, j)] for j in range(n_features)])
-                - gp.quicksum([estimate_y[(i, j)] for j in range(n_features)])
+                        if self.inflexions[j][k] <= Y[i][j] <= self.inflexions[j][k + 1]:
+                            self.solver.addConstr(
+                                self.estimate_y[name, i, j]
+                                == self.marginal_coeffs[name, j, k]
+                                + (
+                                    (Y[i][j] - self.inflexions[j][k])
+                                    / (self.inflexions[j][k + 1] - self.inflexions[j][k])
+                                )
+                                * (self.marginal_coeffs[name, j, k + 1] - self.marginal_coeffs[name, j, k]),
+                                name=f"estimate_y_{i}_{j}",
+                            )
+            
+        pref_d1 = {
+            ("d1", i): self.solver.addConstr(
+                gp.quicksum([self.estimate_x[("d1", i, j)] for j in range(n_features)])
+                - gp.quicksum([self.estimate_y[("d1", i, j)] for j in range(n_features)])
+                - majoring_value * self.z_d[i]
                 <= self.epsilon
             )
             for i in range(n_samples)
         }
-        pref_B = {
-            (i): self.solver.addConstr(
-                - gp.quicksum([estimate_x[(i, j)] for j in range(n_features)])
-                + gp.quicksum([estimate_y[(i, j)] for j in range(n_features)])
+        pref_d2 = {
+            ("d2", i): self.solver.addConstr(
+                gp.quicksum([self.estimate_x[("d2", i, j)] for j in range(n_features)])
+                - gp.quicksum([self.estimate_y[("d2", i, j)] for j in range(n_features)])
+                - majoring_value * (1 - self.z_d[i])
                 <= self.epsilon
             )
             for i in range(n_samples)
         }
 
-        for j in range(self.n_pieces + 1):
-            for i in range(n_features):
-                self.solver.addConstr(self.abs_vals[(i, j)] >= self.marginal_coeffs[(i, j)] - ref_coeffs[(i, j)], name=f"a_{i}_{j}")
-                self.solver.addConstr(self.abs_vals[(i, j)] >= - self.marginal_coeffs[(i, j)] + ref_coeffs[(i, j)], name=f"b_{i}_{j}")
-                self.solver.addConstr(self.abs_vals[(i, j)] <=  self.marginal_coeffs[(i, j)] - ref_coeffs[(i, j)] + majoring_value * self.abs_id[(i, j)], name=f"c_{i}_{j}")
-                self.solver.addConstr(self.abs_vals[(i, j)] <=  -self.marginal_coeffs[(i, j)] + ref_coeffs[(i, j)] + majoring_value * (1 - self.abs_id[(i, j)]), name=f"d_{i}_{j}")
+        pref_s1 = {
+            ("s1", i): self.solver.addConstr(
+                gp.quicksum([self.estimate_x[("s1", i, j)] for j in range(n_features)])
+                - gp.quicksum([self.estimate_y[("s1", i, j)] for j in range(n_features)])
+                - majoring_value * self.z_s[i]
+                <= self.epsilon
+            )
+            for i in range(n_samples)
+        }
+        pref_s2 = {
+            ("s2", i): self.solver.addConstr(
+                gp.quicksum([self.estimate_x[("s2", i, j)] for j in range(n_features)])
+                - gp.quicksum([self.estimate_y[("s2", i, j)] for j in range(n_features)])
+                - majoring_value * (1 - self.z_s[i])
+                <= self.epsilon
+            )
+            for i in range(n_samples)
+        }
+
+        for name in [("d1", "s1"), ("d1", "s2"), ("d2", "s1"), ("d2", "s2")]:
+            for j in range(self.n_pieces + 1):
+                for i in range(n_features):
+                    self.solver.addConstr(self.abs_vals[(f"{name[0]}_{name[1]}", i, j)] >= self.marginal_coeffs[(name[0], i, j)] - self.marginal_coeffs[(name[1], i, j)], name=f"a_{i}_{j}")
+                    self.solver.addConstr(self.abs_vals[(f"{name[0]}_{name[1]}", i, j)] >= - self.marginal_coeffs[(name[0], i, j)] + self.marginal_coeffs[(name[1], i, j)], name=f"b_{i}_{j}")
+                    self.solver.addConstr(self.abs_vals[(f"{name[0]}_{name[1]}", i, j)] <=  self.marginal_coeffs[(name[0], i, j)] - self.marginal_coeffs[(name[1], i, j)] + majoring_value * self.abs_id[(f"{name[0]}_{name[1]}", i, j)], name=f"c_{i}_{j}")
+                    self.solver.addConstr(self.abs_vals[(f"{name[0]}_{name[1]}", i, j)] <=  - self.marginal_coeffs[(name[0], i, j)] + self.marginal_coeffs[(name[1], i, j)] + majoring_value * (1 - self.abs_id[(f"{name[0]}_{name[1]}", i, j)]), name=f"d_{i}_{j}")
 
         ### Obj
 
-        if sample_weight is not None:
-            self.solver.setObjective(
-                gp.quicksum(sigma_err[i] * sample_weight[i] for i in range(n_samples)),
-                gp.GRB.MINIMIZE,
+        distance = self.solver.addVar(
+                vtype=gp.GRB.CONTINUOUS, name=f"D"
             )
-        else:
-            self.solver.setObjective(
-                gp.quicksum(self.abs_vals[(i, j)] for i in range(n_features) for j in range(self.n_pieces+1)), gp.GRB.MAXIMIZE
-            )
+        self.solver.addConstr(distance <= gp.quicksum(self.abs_vals[("d1_s1", i, j)] for i in range(n_features) for j in range(self.n_pieces+1)) + gp.quicksum(self.abs_vals[("d2_s2", i, j)] for i in range(n_features) for j in range(self.n_pieces+1)), name=f"xyz")
+        self.solver.addConstr(distance <= gp.quicksum(self.abs_vals[("d2_s1", i, j)] for i in range(n_features) for j in range(self.n_pieces+1)) + gp.quicksum(self.abs_vals[("d1_s2", i, j)] for i in range(n_features) for j in range(self.n_pieces+1)), name=f"xyz")
+
+        self.solver.setObjective(
+            distance, gp.GRB.MAXIMIZE
+        )
         self.solver.update()
 
         # -- Résolution --
-        print("Optimize")
         self.solver.optimize()
         self.status = self.solver.Status
-        print(self.status)
 
-        if self.status == 2:
-            self.coeffs = [
-                [self.marginal_coeffs[(i, j)].x for j in range(self.n_pieces + 1)]
+        """if self.status == 2:
+            self.lower_bound_coeffs = [
+                [self.lower_bound_marginal_coeffs[(i, j)].x for j in range(self.n_pieces + 1)]
                 for i in range(n_features)
             ]
-
+            self.higher_bound_coeffs = [
+                [self.upper_bound_marginal_coeffs[(i, j)].x for j in range(self.n_pieces + 1)]
+                for i in range(n_features)
+            ]
+            '''
             for k, v in estimate_x.items():
                 estimate_x[k] = v.x
             for k, v in estimate_y.items():
                 estimate_y[k] = v.x
 
-            return estimate_x, estimate_y
+            return estimate_x, estimate_y'''
+            return
         else:
             return "error"
+
+        """
