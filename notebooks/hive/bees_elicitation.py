2aef8102fb896f1f2227f04445ae9750ef5ac2e8
VincentAuriau <auriau.vincent@gmail.com>
2025-12-18 11:02:41 +0100

    fix import

--- notebooks/hive/bees_elicitation.py
+++ notebooks/hive/bees_elicitation.py
@@ -13,7 +13,7 @@ import numpy as np
 from python.data_generation import SyntheticDataGenerator
 from python.distances import TwoUTASpaceDiameter
 
-from .host_config import get_xp_savedir
+from host_config import get_xp_savedir
 
 
 logger = logging.getLogger(__name__)
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     # main_save_dir = "/gpfs/workdir/auriauvi/honey"
     # main_save_dir = "/data/workspace/vincent/elicit"
     main_save_dir = get_xp_savedir()
-    n_runs = 2
+    n_runs = 10
     n_data = [32, 128, 512, 2048]
     time_limit = 10_800
     inflexions = np.vstack([np.linspace(0, 1., 6)] * 4)
