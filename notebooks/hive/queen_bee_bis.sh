33e2deffcbf89a9ef814d92dd7cb8330ca43a883
Vincent Auriau <auriau.vincent@gmail.com>
2026-01-13 08:55:37 +0100

    few modifs (#8)

--- notebooks/hive/queen_bee_bis.sh
+++ notebooks/hive/queen_bee_bis.sh
@@ -15,4 +15,4 @@ module load python/3.9.10/gcc-11.2.0
 source /gpfs/users/auriauvi/.venv/guro/bin/activate
 cd /gpfs/users/auriauvi/eliciting-discrete-mixtures-of-UTAs/notebooks/hive
 
-python -u bees_elicitation.py --flag b
+python -u bees_elicitation.py --flag e
